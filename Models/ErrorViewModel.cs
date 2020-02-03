35089e745ae7a02f678b7d3eb348488d6af4f6dc
BICapacitacion <BICapacitacion@L3-2-CAP1>
2020-02-03 16:54:02 -0600

    guardar

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pruebas2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
